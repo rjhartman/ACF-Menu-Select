95732a3ae6070dfd18c5b00201b6e5e642a41b3e
Ryan Hartman <rhartman1239@gmail.com>
2020-09-24 12:52:06 -0400

    Cleanup

--- acf-menu-select.php
+++ acf-menu-select.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: Advanced Custom Fields: Menu Selector
-Plugin URI: https://gitlab.com/nextlevelweb/menu-select.git
+Plugin URI: https://github.com/RHartman99/ACF-Menu-Select
 Description: Adds a WP menu selector field type to ACF.
 Version: 1.0.0
 Author: Ryan Hartman
@@ -12,19 +12,20 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 // exit if accessed directly
-if( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 
 // check if class already exists
-if( !class_exists('nextlevel_acf_plugin_menu') ) :
+if (!class_exists('nextlevel_acf_plugin_menu')) :
 
-class nextlevel_acf_plugin_menu {
-	
-	// vars
-	var $settings;
-	
-	
-	/*
+	class nextlevel_acf_plugin_menu
+	{
+
+		// vars
+		var $settings;
+
+
+		/*
 	*  __construct
 	*
 	*  This function will setup the class functionality
@@ -36,25 +37,26 @@ class nextlevel_acf_plugin_menu {
 	*  @param	void
 	*  @return	void
 	*/
-	
-	function __construct() {
-		
-		// settings
-		// - these will be passed into the field class.
-		$this->settings = array(
-			'version'	=> '1.0.0',
-			'url'		=> plugin_dir_url( __FILE__ ),
-			'path'		=> plugin_dir_path( __FILE__ )
-		);
-		
-		
-		// include field
-		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
-		add_action('acf/register_fields', 		array($this, 'include_field')); // v4
-	}
-	
-	
-	/*
+
+		function __construct()
+		{
+
+			// settings
+			// - these will be passed into the field class.
+			$this->settings = array(
+				'version'	=> '1.0.0',
+				'url'		=> plugin_dir_url(__FILE__),
+				'path'		=> plugin_dir_path(__FILE__)
+			);
+
+
+			// include field
+			add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
+			add_action('acf/register_fields', 		array($this, 'include_field')); // v4
+		}
+
+
+		/*
 	*  include_field
 	*
 	*  This function will include the field type class
@@ -66,29 +68,27 @@ class nextlevel_acf_plugin_menu {
 	*  @param	$version (int) major ACF version. Defaults to false
 	*  @return	void
 	*/
-	
-	function include_field( $version = false ) {
-		
-		// support empty $version
-		if( !$version ) $version = 4;
-		
-		
-		// load acf-menu-select
-		load_plugin_textdomain( 'acf-menu-select', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' ); 
-		
-		
-		// include
-		include_once('fields/class-nextlevel-acf-field-menu-v' . $version . '.php');
+
+		function include_field($version = false)
+		{
+
+			// support empty $version
+			if (!$version) $version = 4;
+
+
+			// load acf-menu-select
+			load_plugin_textdomain('acf-menu-select', false, plugin_basename(dirname(__FILE__)) . '/lang');
+
+
+			// include
+			include_once('fields/class-nextlevel-acf-field-menu-v' . $version . '.php');
+		}
 	}
-	
-}
 
 
-// initialize
-new nextlevel_acf_plugin_menu();
+	// initialize
+	new nextlevel_acf_plugin_menu();
 
 
 // class_exists check
 endif;
-	
-?>
